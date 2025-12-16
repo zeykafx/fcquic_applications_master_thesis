6d9525d58d279d6e78e681922c8d3273e4685aba
Corentin Detry <corentin.detry@gmail.com>
2025-12-16 22:34:18 +0100

    npf script updates

--- evaluations/run_test.sh
+++ evaluations/run_test.sh
@@ -1,3 +1,7 @@
 echo "Running latency test"
 
-npf-run --test ./tests/latency/script.npf --cluster client=localhost server=localhost --single-output ./tests/latency/npf_out.csv --no-graph
+./venv/bin/npf-run --test ./tests/latency/script.npf \
+    --single-output ./tests/latency/npf_out.csv \
+    --no-graph --force-retest \
+    --variables workdir=$(pwd)/.. \
+    cargo_path="/home/corentin/.cargo/bin/cargo"
