fca3482b04cbba1e82eb1b9d5437a5a0fdc5100b
Corentin Detry <corentin.detry@gmail.com>
2025-12-25 16:11:12 +0100

    added graph notebook, updates to npf script

--- evaluations/run_test.sh
+++ evaluations/run_test.sh
@@ -1,12 +1,59 @@
-echo "Running latency test"
-CARGO_PATH=$(which cargo)
+IS_BASELINE=${1:-false} # set IS_BASELINE to true if the first arg is true, otherwise set it to false
 
+# https://stackoverflow.com/a/13864829
+if [ -z "${1+x}" ]; then
+	# no arg is passed, do nothing
+	echo "No arg passed in, running fcquic version"
+else
+	shift # remove the first argument from the list
+fi
+
+CARGO_PATH=$(which cargo)
+workdir=$(pwd)/..
+
+if [ "$IS_BASELINE" = "true" ]; then
+    echo "Running latency test with BASELINE QUIC version"
+else
+    echo "Running latency test"
+fi
+
+# Killing any server or client application still running
+sudo pkill -f "sudo ip netns exec client" && sudo pkill -f "sudo ip netns exec server"
+
+# ---------------- Logs setup ----------------
+echo "Setting up logs directory"
+
+cd $workdir/evaluations/tests/latency
+
+sudo mkdir ./logs 2> /dev/null
+DIR=./logs
+
+PREV_RUN_NBR=$(find ${DIR} -maxdepth 1 -type d -regex '.*/[0-9]+' -printf '%f\n' 2>/dev/null | sort -n | tail -n 1)
+PREV_RUN_NBR=${PREV_RUN_NBR:-0}
+CUR_RUN=$(echo "${PREV_RUN_NBR}+1" | bc)
+LOGS_BASE_DIR=${DIR}/${CUR_RUN}
+
+# append "baseline" to the dir name if running the baseline test
+if [ "$IS_BASELINE" = "true" ]; then
+	LOGS_BASE_DIR="${LOGS_BASE_DIR}_baseline"
+else
+	LOGS_BASE_DIR="${LOGS_BASE_DIR}_fcquic"
+fi
+
+sudo mkdir ${LOGS_BASE_DIR}
+RUN_LOGS_DIR=${LOGS_BASE_DIR}
+
+cd $workdir/evaluations
+
+# ---------------- Running npf script ----------------
 
 sudo -E ./venv/bin/npf-run --test ./tests/latency/script.npf \
     --single-output ./tests/latency/out/npf_out.csv \
     --no-graph --force-retest \
-    --variables workdir=$(pwd)/.. \
+    --variables workdir=$workdir \
     cargo_path=$CARGO_PATH \
+    RUN_LOGS_DIR=$RUN_LOGS_DIR \
+    IS_BASELINE=$IS_BASELINE \
     $@
-    
+
 # The line "$@" allows us to pass the remaning arguments from this script to the npf script
--- evaluations/run_test_baseline.sh
+++ evaluations/run_test_baseline.sh
@@ -1,12 +1,17 @@
-echo "Running latency test with BASELINE QUIC version"
-CARGO_PATH=$(which cargo)
+# echo "Running latency test with BASELINE QUIC version"
+# CARGO_PATH=$(which cargo)
 
-sudo -E ./venv/bin/npf-run --test ./tests/latency/script.npf \
-    --single-output ./tests/latency/out/npf_out_baseline.csv \
-    --no-graph --force-retest \
-    --variables workdir=$(pwd)/.. \
-    IS_BASELINE=true \
-    cargo_path=$CARGO_PATH \
-    $@
+# # Killing any server or client application still running
+# sudo pkill -f "sudo ip netns exec client" && sudo pkill -f "sudo ip netns exec server"
 
-# The line "$@" allows us to pass the remaning arguments from this script to the npf script
+# sudo -E ./venv/bin/npf-run --test ./tests/latency/script.npf \
+#     --single-output ./tests/latency/out/npf_out_baseline.csv \
+#     --no-graph --force-retest \
+#     --variables workdir=$(pwd)/.. \
+#     IS_BASELINE=true \
+#     cargo_path=$CARGO_PATH \
+#     $@
+
+# # The line "$@" allows us to pass the remaning arguments from this script to the npf script
+
+./run_test.sh true
