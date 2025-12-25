12b6e806095f9947e79025c0a6010da8103a32ab
Corentin Detry <corentin.detry@gmail.com>
2025-12-25 12:29:22 +0100

    updates to npf script, ran test for baseline and fcquic

--- evaluations/run_test_baseline.sh
+++ evaluations/run_test_baseline.sh
@@ -1,12 +1,12 @@
-echo "Running latency test"
+echo "Running latency test with BASELINE QUIC version"
 CARGO_PATH=$(which cargo)
 
-
 sudo -E ./venv/bin/npf-run --test ./tests/latency/script.npf \
-    --single-output ./tests/latency/out/npf_out.csv \
+    --single-output ./tests/latency/out/npf_out_baseline.csv \
     --no-graph --force-retest \
     --variables workdir=$(pwd)/.. \
+    IS_BASELINE=true \
     cargo_path=$CARGO_PATH \
     $@
-    
+
 # The line "$@" allows us to pass the remaning arguments from this script to the npf script
